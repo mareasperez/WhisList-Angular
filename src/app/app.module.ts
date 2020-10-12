4d1f4cc965e66bfe114b09e72d510648e178044e
Maury <mareasdperez7@gmail.com>
2020-10-11 19:46:13 -0600

    leccion 3: integrando Redux y aAgular parte 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,26 +8,46 @@ import { DestinoDetalleComponent } from './destino-detalle/destino-detalle.compo
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FormDestinoViajeComponent } from './form-destino-viaje/form-destino-viaje.component';
 import { DestinosApiClient } from 'src/app/modelos/destinos-api-client.model';
+import {
+  DestinosViajesState,
+  reducerDestinosViajes,
+  intializeDestinosViajesState,
+  DestinosViajesEffects
+} from './modelos/destinos-viajes-state.model';
+import { ActionReducerMap, StoreModule as NgRxStoreModule, ActionReducer } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: ListaDestinosComponent },
   { path: 'destino', component: DestinoDetalleComponent },
 
-]
+];
+// Redux
+export interface AppState {
+  destinos: DestinosViajesState;
+}
+const reducers: ActionReducerMap<AppState> = {
+  destinos: reducerDestinosViajes
+};
+const reducersInitialState = {
+  destinos: intializeDestinosViajesState()
+};
 @NgModule({
   declarations: [
     AppComponent,
     DestinoViajeComponent,
     ListaDestinosComponent,
     DestinoDetalleComponent,
-    FormDestinoViajeComponent
+    FormDestinoViajeComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
     RouterModule.forRoot(routes),
+    NgRxStoreModule.forRoot(reducers, { initialState: reducersInitialState }),
+    EffectsModule.forRoot([DestinosViajesEffects])
   ],
   providers: [DestinosApiClient],
   bootstrap: [AppComponent]
--- src/app/modelos/detino-viaje.model.ts
+++ src/app/modelos/detino-viaje.model.ts
@@ -3,9 +3,16 @@ export class DestinoViaje {
     public servicios: string[];
     private selected: boolean;
     public id = uuid();
+    public votes = 0;
     constructor(public nombre: string, public url: string) {
         this.servicios = ['pileta', 'desayuno'];
     }
     isSelected(): boolean { return this.selected; }
     setSelected(value: boolean) { this.selected = value; }
+    voteUp(): any {
+        this.votes++;
+    }
+    voteDown(): any {
+        this.votes--;
+    }
 }
