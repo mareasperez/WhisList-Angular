a98930fa6368ed74b778471e46e9b99b29a177a5
Maury <mareasdperez7@gmail.com>
2020-10-11 20:12:29 -0600

    Leccion 3 integracion con redux parte 2

--- src/app/lista-destinos/lista-destinos.component.ts
+++ src/app/lista-destinos/lista-destinos.component.ts
@@ -1,21 +1,25 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { DestinoViaje } from 'src/app/modelos/detino-viaje.model';
 import { DestinosApiClient } from 'src/app/modelos/destinos-api-client.model';
+import { Store } from '@ngrx/store';
+import { AppState } from '../app.module';
+import { ElegidoFavoritoAction, NuevoDestinoAction } from '../modelos/destinos-viajes-state.model';
 @Component({
   selector: 'app-lista-destinos',
   templateUrl: './lista-destinos.component.html',
   styleUrls: ['./lista-destinos.component.css']
 })
 export class ListaDestinosComponent implements OnInit {
+  // tslint:disable-next-line: no-output-on-prefix
   @Output() onItemAdded: EventEmitter<DestinoViaje>;
   updates: string[];
-  constructor(private destinosApiClient: DestinosApiClient) {
+  constructor(private destinosApiClient: DestinosApiClient, private store: Store<AppState>) {
     this.onItemAdded = new EventEmitter<DestinoViaje>();
     this.updates = [];
-    this.destinosApiClient.subscribeOnChange((d: DestinoViaje) => {
-        if (d != null) {
-            this.updates.push('destino actualizado: ' + d.nombre);
-        }
+    this.store.select(state => state.destinos.favorito).subscribe(data => {
+         if (data != null) {
+        this.updates.push('destino actualizado: ' + data.nombre);
+      }
     });
   }
 
@@ -24,8 +28,10 @@ export class ListaDestinosComponent implements OnInit {
   agregado(destino: DestinoViaje) {
     this.destinosApiClient.add(destino);
     this.onItemAdded.emit(destino);
+    this.store.dispatch(new NuevoDestinoAction(destino));
   }
   elegido(destino: DestinoViaje) {
     this.destinosApiClient.elegir(destino);
+    this.store.dispatch(new ElegidoFavoritoAction(destino));
   }
 }
